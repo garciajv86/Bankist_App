b8885f03ae0618236ebcd97d7bae118c155830a4
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2024-12-14 14:35:16 -0600

    added event listener logic for the request loan form

--- script.js
+++ script.js
@@ -197,6 +197,21 @@ btnTransfer.addEventListener("click", e => {
   }
 });
 
+btnLoan.addEventListener("click", e => {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = "";
+});
+
 btnClose.addEventListener("click", e => {
   e.preventDefault();
 
